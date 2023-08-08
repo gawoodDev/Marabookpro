bab6fa241f71a3bbee5ac8629a6eea7bbcd717f5
accesorieswebdev@gmail.com <accesorieswebdev@gmail.com>
2023-08-08 10:49:35 -0400

    redisign et ajustement des page

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+let script_content = document.getElementById('box_script_text_content')
+let switch_script = document.getElementById('switch_script')
 
 
-console.log('Hello World!');
 
 
-
-
+switch_script.addEventListener('click', (e)=>{
+  e.preventDefault()
+  
+  
+  script_content.classList.toggle('open')
+  
+})
