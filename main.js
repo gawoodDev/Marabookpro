cd0b7aa64c09b9a49eb086d091dcaf107e2d9c7b
accesorieswebdev@gmail.com <accesorieswebdev@gmail.com>
2023-09-05 19:00:17 -0400

    commit to

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
-let script_content = document.getElementById('box_script_text_content')
-let switch_script = document.getElementById('switch_script')
 
 
+let menu_button = document.querySelector('.menu_button')
+let box_nav_head = document.querySelector('.box_nav_head')
+let closer_nav = document.querySelector('.closer_nav')
 
+closer_nav.addEventListener('click', (e)=>{
+  box_nav_head.classList.toggle('open')
+})
 
-switch_script.addEventListener('click', (e)=>{
+menu_button.addEventListener('click', (e)=>{
   e.preventDefault()
   
-  
-  script_content.classList.toggle('open')
-  
+  box_nav_head.classList.toggle('open')
+
+
 })
+
+
+
+
+
+
+
